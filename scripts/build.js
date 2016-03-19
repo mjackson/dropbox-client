7919c43df0c7d6183035208e07b760255f71e8a4
Michael Jackson <mjijackson@gmail.com>
2016-03-18 18:42:25 -0700

    Add browser build + release scripts

---
+++ scripts/build.js
@@ -0,0 +1,17 @@
+const readFileSync = require('fs').readFileSync
+const execSync = require('child_process').execSync
+const prettyBytes = require('pretty-bytes')
+const gzipSize = require('gzip-size')
+
+const exec = (command) =>
+  execSync(command, { stdio: 'inherit' })
+
+exec('npm run build-cjs')
+exec('npm run build-umd')
+exec('npm run build-min')
+
+console.log(
+  '\ngzipped, the UMD build is ' + prettyBytes(
+    gzipSize.sync(readFileSync('umd/dropbox-client.min.js'))
+  )
+)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack')
+
+module.exports = {
+
+  output: {
+    library: 'DropboxClient',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+
+  plugins: [
+    new webpack.IgnorePlugin(/node-fetch/),
+    new webpack.DefinePlugin({
+      'typeof window': JSON.stringify('object')
+    })
+  ]
+
+}
